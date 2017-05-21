be199143ae93eb106298adf68f00d6f164ab4df7
森田賢二 <kenjimoritata@gmail.com>
2017-05-21 11:17:06 +0900

    Create bean

--- MySpringApp1/src/main/java/jp/morita/spring/sample1/App.java
+++ MySpringApp1/src/main/java/jp/morita/spring/sample1/App.java
@@ -1,11 +1,14 @@
 package jp.morita.spring.sample1;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		MyBean bean = new MyBean("kenji morita!");
+		ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("bean.xml");
+		MyBean bean = (MyBean)app.getBean("mybean1");
 		System.out.println(bean);
 	}
 
