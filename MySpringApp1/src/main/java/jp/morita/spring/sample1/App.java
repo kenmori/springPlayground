33aa6568bbdb0068ebe9f15f5d08b1b9dc660ea2
森田賢二 <kenjimoritata@gmail.com>
2017-05-21 11:20:42 +0900

    fix

--- MySpringApp1/src/main/java/jp/morita/spring/sample1/App.java
+++ MySpringApp1/src/main/java/jp/morita/spring/sample1/App.java
@@ -1,13 +1,14 @@
 package jp.morita.spring.sample1;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-
+	private static ApplicationContext app;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("bean.xml");
+		app = new ClassPathXmlApplicationContext("bean.xml");
 		MyBean bean = (MyBean)app.getBean("mybean1");
 		System.out.println(bean);
 	}
